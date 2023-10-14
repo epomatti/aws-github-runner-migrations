aa584ed5ebcc57336dc385c3ca42862b0e81cc55
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-10-13 22:36:10 -0300

    clean

--- app/index.ts
+++ app/index.ts
@@ -6,6 +6,23 @@ import { PrismaClient } from '@prisma/client'
 
   const prisma = new PrismaClient();
 
+  // Credits: https://stackoverflow.com/a/14032965/3231778
+  async function exitHandler(options: any, exitCode: any) {
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    await prisma.$disconnect()
+    if (options.exit) process.exit();
+  }
+  // do something when app is closing
+  process.on('exit', exitHandler.bind(null, { cleanup: true }));
+  // catches ctrl+c event
+  process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+  // catches "kill pid" (for example: nodemon restart)
+  process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+  process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+  // catches uncaught exceptions
+  process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+
   try {
     process.on("uncaughtException", function (err) {
       console.error(err);
